a926069f59fd5c26fa3e4a5bc93123546c21a027
Condrey <wangzdrey@gmail.com>
2025-02-17 20:52:39 +0300

    w-full

--- src/components/navigation-bar/asset-management-nav-bar.tsx
+++ src/components/navigation-bar/asset-management-nav-bar.tsx
@@ -41,7 +41,7 @@ export default function AssetManagementNavBar({
   const isHomeActive = pathname.toString() === basePathname;
 
   return (
-    <div className="flex sticky top-0 w-full items-center justify-start bg-background border-b px-4 py-2 lg:px-6">
+    <div className="flex z-50 sticky top-0 w-full items-center justify-start bg-background border-b px-4 py-2 lg:px-6">
       <NavigationMenu>
         <NavigationMenuList className={className}>
           {/* dashboard nav */}
