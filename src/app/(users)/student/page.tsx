f49e924095d66fd4388dc6dce684282b11a417d7
Condrey <wangzdrey@gmail.com>
2025-03-07 12:20:32 +0300

    rethink

--- src/app/(users)/student/page.tsx
+++ src/app/(users)/student/page.tsx
@@ -5,21 +5,21 @@ import { getUser } from "./action";
 import UserDetails from "./user-details/user-details";
 
 const getLoggedUser = cache(getUser);
-
+// TODO: rethink 
 export async function generateMetadata(
-  parent: ResolvingMetadata,
+  // parent: ResolvingMetadata,
 ): Promise<Metadata> {
   const { user: pupil } = await getLoggedUser();
   if (!pupil) return notFound();
   const { name, email, telephone, username, avatarUrl } = pupil;
-  const previousImages = (await parent).openGraph?.images || [];
+  // const previousImages = (await parent).openGraph?.images || [];
 
   return {
     title: `${name} - ${telephone || email || `$${username}`}`,
     description: `Details of ${name} - ${telephone || email || `$${username}`}, including fees payments and class streams attended over the years`,
-    openGraph: {
-      images: !avatarUrl ? previousImages : [avatarUrl, ...previousImages],
-    },
+    // openGraph: {
+    //   images: !avatarUrl ? previousImages : [avatarUrl, ...previousImages],
+    // },
   };
 }
 
