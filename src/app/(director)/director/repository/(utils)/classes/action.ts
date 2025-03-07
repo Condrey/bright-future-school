ac3a455404b6feb9ee4e1b74e3092b6d9af605c6
Condrey <wangzdrey@gmail.com>
2025-03-07 12:23:53 +0300

    slug for class

--- src/app/(director)/director/repository/(utils)/classes/action.ts
+++ src/app/(director)/director/repository/(utils)/classes/action.ts
@@ -2,6 +2,7 @@
 
 import prisma from "@/lib/prisma";
 import { classDataInclude } from "@/lib/types";
+import { slugify } from "@/lib/utils";
 import { classSchema, ClassSchema } from "@/lib/validation";
 
 export async function getClassesAction() {
@@ -12,24 +13,26 @@ export async function getClassesAction() {
   return classes;
 }
 export async function addClassAction(input: ClassSchema) {
-  const { name, level } = classSchema.parse(input);
+  const { name, level, slug } = classSchema.parse(input);
 
   const data = await prisma.class.create({
     data: {
       name,
       levelId: level.id,
+      slug:slugify(slug)
     },
   });
   return data;
 }
 
 export async function editClassAction(input: ClassSchema) {
-  const { name, id, level } = classSchema.parse(input);
+  const { name, id, level,slug } = classSchema.parse(input);
   const data = await prisma.class.update({
     where: { id },
     data: {
       name,
       levelId: level.id,
+      slug:slugify(slug)
     },
   });
   return data;
